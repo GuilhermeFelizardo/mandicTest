aca9891e6587dbd491632aa3d0d0ee88fbda3cb0
GuilhermeFelizardo <guilherme_f.reis@hotmail.com>
2020-01-28 14:04:28 -0300

    teste deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Initial commit test on continuous deployment"/>
   </div>
 </template>
 
